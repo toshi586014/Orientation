91f8a3b6b3bf5109514b6612696ffa49869c1bd8
toshi586014 <toshi586014@icloud.com>
2016-02-22 15:21:44 +0900

    Initial Commit

---
+++ Orientation/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Orientation
+//
+//  Created by 中村俊昭 on 2016/02/22.
+//  Copyright © 2016年 Toshiaki Nakamura. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
